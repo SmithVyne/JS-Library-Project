084f40dd097dbdeda6c0116ad7363816dd16b329
Ezaka Alfred <ezakaphil@gmail.com>
2020-09-16 22:32:22 +0100

    fit github linter warnings

--- script.js
+++ script.js
@@ -3,41 +3,52 @@ const dummyBooks = [
     author: 'Mark Twain',
     title: 'Animal Farm',
     pages: 300,
-    read: true
+    read: true,
   },
   {
     author: 'Jon Doe',
     title: 'The Ring',
     pages: 200,
-    read: false
+    read: false,
   },
   {
     author: 'Paul West',
     title: 'John\'s travels',
     pages: 200,
-    read: true
-  }
+    read: true,
+  },
 ]
 
-library = [...dummyBooks];
+const library = [...dummyBooks];
 
-
-function Book (title, author, pages, read) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 
-  this.info = function() {
-    return title + ', ' + author + ', ' + pages + ', ' + read;
-  }
+  this.info = function () {
+    return `${title} ${author} ${pages} ${read}`;
+  };
 }
 
+const validateBook = (book) => {
+  const errors = [];
+  if(book.author == '') {
+    errors.push('Book author cannot be empty');
+  } else if(book.title == '') {
+    errors.push('Book title cannot be empty');
+  } else if(book.pages == '') {
+    errors.push('Number of Pages cannot be empty')
+  }
+
+  return errors;
+}
 
 function addBook() {
-  const btnAddbook = document.getElementById("btnAddbook");
+  const btnAddbook = document.getElementById('btnAddbook');
 
-  btnAddbook.addEventListener('click', ()=>{
+  btnAddbook.addEventListener('click', () => {
     const title = document.getElementById('title');
     const author = document.getElementById('author');
     const pages = document.getElementById('pages');
@@ -49,19 +60,19 @@ function addBook() {
       read = false;
     }
 
-    let newBook = new Book(title.value, author.value, pages.value, read);
+    const newBook = new Book(title.value, author.value, pages.value, read);
     // Validate New Book
     const validationResult = validateBook(newBook);
-    let errorField = document.createElement('div');
-    
-    if(validationResult.length > 0) {
-        const container = document.querySelector(".container-fluid");
-        errorField.classList.add('error-field');
+    const errorField = document.createElement('div');
 
-        validationResult.forEach((err) => {
-          errorField.textContent += err;
-        })
-        container.appendChild(errorField, container);
+    if (validationResult.length > 0) {
+      const container = document.querySelector(".container-fluid");
+      errorField.classList.add('error-field');
+
+      validationResult.forEach((err) => {
+        errorField.textContent += err;
+      })
+      container.appendChild(errorField, container);
       return
     } else {
       errorField.setAttribute('style', 'display: none;');
@@ -77,24 +88,24 @@ function addBook() {
 }
 
 const displayBooks = (library) => {
-  let bookContainer = document.querySelector(".book-container");
-  bookContainer.innerHTML = "";
+  let bookContainer = document.querySelector('.book-container');
+  bookContainer.innerHTML = '';
   
-    for(let i = 0; i < library.length; i++) {
-      let book = library[i];
-      bookContainer.innerHTML += `
-      <div class="card my-2 col-xl-3 col-lg-4 col-sm-12 col-md-6">
-        <div class="card-body">
-          <h5 class="card-title book-title">${book.title}</h5>
-          <p class="card-text book-author">${book.author}</p>
-          <p class="card-text book-pages">${book.pages}</p>
-          <p class="card-text d-flex box-read">
-          <span> Read: ${hasRead(book, i)} </span>
-          </p>
-          <button class="btn btn-danger" onclick="deleteBook(${i})">Delete</button>
-        </div>
-      </div>`;
-    }
+  for(let i = 0; i < library.length; i++) {
+    let book = library[i];
+    bookContainer.innerHTML += `
+    <div class="card my-2 col-xl-3 col-lg-4 col-sm-12 col-md-6">
+      <div class="card-body">
+        <h5 class="card-title book-title">${book.title}</h5>
+        <p class="card-text book-author">${book.author}</p>
+        <p class="card-text book-pages">${book.pages}</p>
+        <p class="card-text d-flex box-read">
+        <span> Read: ${hasRead(book, i)} </span>
+        </p>
+        <button class="btn btn-danger" onclick="deleteBook(${i})">Delete</button>
+      </div>
+    </div>`;
+  };
 }
 
 const hasRead = (book, i) => {
@@ -123,19 +134,6 @@ const deleteBook = ((i) => {
 const openModal = () => {
   const formArea = document.querySelector('#formArea');
   formArea.classList.toggle('d-none');
-}
-
-const validateBook = (book) => {
-  const errors = [];
-  if(book.author == '') {
-    errors.push('Book author cannot be empty');
-  } else if(book.title == '') {
-    errors.push('Book title cannot be empty');
-  } else if(book.pages == '') {
-    errors.push('Number of Pages cannot be empty')
-  }
-
-  return errors;
 }
 
 displayBooks(library)
