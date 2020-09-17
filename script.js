8c8e5232841c084f6575224cc6813ccebacd6d25
SmithVyne <smithnkereuwem2@gmail.com>
2020-09-17 15:35:08 +0300

    add required changes

--- script.js
+++ script.js
@@ -32,19 +32,6 @@ function Book(title, author, pages, read) {
   };
 }
 
-const validateBook = (book) => {
-  const errors = [];
-  if (book.author === '') {
-    errors.push('Book author cannot be empty');
-  } else if (book.title === '') {
-    errors.push('Book title cannot be empty');
-  } else if (book.pages === '') {
-    errors.push('Number of Pages cannot be empty');
-  }
-
-  return errors;
-};
-
 const hasRead = (book, i) => {
   if (book.read) {
     return `<span class="badge badge-success" onclick="changeTrue(${i})">True</span>`;
@@ -53,6 +40,20 @@ const hasRead = (book, i) => {
   return `<span class="badge badge-danger" onclick="changeFalse(${i})">False</span>`;
 };
 
+const innerCard = (bookContainer, book, i) => {
+  bookContainer.innerHTML += `
+  <div class="card my-2 col-xl-3 col-lg-4 col-sm-12 col-md-6">
+    <div class="card-body">
+      <h5 class="card-title book-title">${book.title}</h5>
+      <p class="card-text book-author">${book.author}</p>
+      <p class="card-text book-pages">${book.pages}</p>
+      <p class="card-text d-flex box-read">
+      <span> Read: ${hasRead(book, i)} </span>
+      </p>
+      <button class="btn btn-danger" onclick="deleteBook(${i})">Delete</button>
+    </div>
+  </div>`;
+};
 
 const displayBooks = (library) => {
   const bookContainer = document.querySelector('.book-container');
@@ -60,18 +61,7 @@ const displayBooks = (library) => {
 
   for (let i = 0; i < library.length; i++) {
     const book = library[i];
-    bookContainer.innerHTML += `
-    <div class="card my-2 col-xl-3 col-lg-4 col-sm-12 col-md-6">
-      <div class="card-body">
-        <h5 class="card-title book-title">${book.title}</h5>
-        <p class="card-text book-author">${book.author}</p>
-        <p class="card-text book-pages">${book.pages}</p>
-        <p class="card-text d-flex box-read">
-        <span> Read: ${hasRead(book, i)} </span>
-        </p>
-        <button class="btn btn-danger" onclick="deleteBook(${i})">Delete</button>
-      </div>
-    </div>`;
+    innerCard(bookContainer, book, i);
   }
 };
 
@@ -95,48 +85,56 @@ const openModal = () => {
   formArea.classList.toggle('d-none');
 };
 
+const resetForm = (title, author, pages, box) => {
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  box.checked = false;
+};
+
+const validateBook = (book) => {
+  const errors = [];
+  if (book.author === '') {
+    errors.push('Book author cannot be empty');
+  } else if (book.title === '') {
+    errors.push('Book title cannot be empty');
+  } else if (book.pages === '') {
+    errors.push('Number of Pages cannot be empty');
+  }
+  return errors;
+};
+
+const validation = (newBook) => {
+  const validationResult = validateBook(newBook);
+  const errorField = document.querySelector('.errors');
+
+  if (validationResult.length > 0) {
+    errorField.classList.add('error-field');
+    errorField.textContent = '';
+    validationResult.forEach((err) => {
+      errorField.textContent += err;
+    });
+    return false;
+  }
+  errorField.classList.add('d-none');
+  return true;
+};
+
 function addBook() {
-  const btnAddbook = document.getElementById('btnAddbook');
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const pages = document.getElementById('pages');
+  const box = document.querySelector('#check-book');
+  const read = !!box.checked;
 
-  btnAddbook.addEventListener('click', () => {
-    const title = document.getElementById('title');
-    const author = document.getElementById('author');
-    const pages = document.getElementById('pages');
-    const box = document.querySelector('#check-book');
-    let read;
-    if (box.checked) {
-      read = true;
-    } else {
-      read = false;
-    }
-
-    const newBook = new Book(title.value, author.value, pages.value, read);
-    // Validate New Book
-    const validationResult = validateBook(newBook);
-    const errorField = document.createElement('div');
-
-    if (validationResult.length > 0) {
-      const container = document.querySelector('.container-fluid');
-      errorField.classList.add('error-field');
-
-      validationResult.forEach((err) => {
-        errorField.textContent += err;
-      });
-      return container.appendChild(errorField);
-    }
-
-    errorField.classList.add('d-none');
+  const newBook = new Book(title.value, author.value, pages.value, read);
+  if (validation(newBook)) {
     library.push(newBook);
-    displayBooks(library);
+  }
 
-    title.value = '';
-    author.value = '';
-    pages.value = '';
-    read.value = '';
-
-    return newBook;
-  });
+  displayBooks(library);
+  resetForm(title, author, pages, box);
+  return newBook;
 }
 
 displayBooks(library);
-addBook();
